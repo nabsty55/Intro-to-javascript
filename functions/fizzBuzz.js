7a606f182158d69a36fab34276ed38cb66f5f9ec
nabsty55 <nabsty55@gmail.com>
2019-03-18 08:03:44 +0300

    fizzbuzz in arrow

--- functions/fizzBuzz.js
+++ functions/fizzBuzz.js
@@ -1,13 +1,16 @@
-var x = 1;
-while (x <= 100){
-  if (x%3===0 && x%5===0){
-    console.log("FizzBuzz");
-  }else if (x%3===0) {
-    console.log("Fizz");
-  }else if (x%5===0){
-    console.log("Buzz");
-  }else{
-    console.log(x);
+//var x = 1;
+const fizzBuzz = (startingNum) => {
+  while (startingNum <= 100){
+    if (startingNum%3===0 && startingNum%5===0){
+      console.log("FizzBuzz");
+    }else if (startingNum%3===0) {
+      console.log("Fizz");
+    }else if (startingNum%5===0){
+      console.log("Buzz");
+    }else{
+      console.log(startingNum);
+    }
+    startingNum ++;
   }
-  x ++;
 }
+console.log(fizzBuzz(1));
