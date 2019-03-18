362d15a25bbe8a048e682686a9027dd90996c38e
nabsty55 <nabsty55@gmail.com>
2019-03-18 09:01:18 +0300

    try100 arrow function

--- functions/try_100.js
+++ functions/try_100.js
@@ -1,6 +1,10 @@
-var n = 1;
-if (n > 100){
-  console.log();
-}else{
-  console.log("Enter another number!");
+//var n = 1;
+const try100 = (n) => {
+  if (n > 100){
+    console.log("Number is above 100");
+  }else{
+    console.log("Enter another number above 100");
+  }
+
 }
+console.log(try100(120));
